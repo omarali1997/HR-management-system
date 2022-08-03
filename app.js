46e8fd4ab4caac859d0b3044762a2c0a4aadceab
YOUR FULL NAMEomarali1997 <omar.aziz.9718@gmail.com>
2022-08-04 00:22:08 +0300

    complete the task9

--- app.js
+++ app.js
@@ -1,146 +1,57 @@
 
+// var id = 999 ;
 
+function salaryemp(max , min) {
 
-
-
-var id = 999 ;
-function salaryemp1(max , min) {
-    return Math.floor(Math.random() * (max - min) ) + min;
-  }
-
-let netSalary = (salaryemp1-(salaryemp1*0.075))
-
-const Emp1 = {
-EmpId:  function(){
-  id++;
-  return id;
-},
-Name: "Ghazi samer",
-Dept: "Administration",
-Level: "Senior",
-salary: 0,
-
-
-emp: function() {
-    // console.log("Employee ID : " + Emp1.EmpId())
-    console.log("Employee name : " + this.Name)
-    console.log("Employee salary : " + salaryemp1(1500,2000))
-}
+  return Math.floor(Math.random() * (max - min) ) + min;
 }
 
-Emp1.emp();
+// function uniqeId(){
+//   id++;
+//   return id;
+// }
 
-const Emp2 = {
-  EmpId:  function(){
-    id++;
-    return id;
-  },
-  Name: "Lana Ali",
-  Dept: "Finance",
-  Level: "Senior",
-  salary: 0,
-  
-  emp: function () {
-    // console.log("Employee ID : " + Emp2.EmpId())
-      console.log("Employee name : " + this.Name)
-      console.log("Employee salary : " + salaryemp1(1500,2000))
-  }
-  }
-  
-  Emp2.emp();
-    
-const Emp3 = {
-  EmpId:  function(){
-    id++;
-    return id;
-  },
-  Name: "Tamara Ayoub",
-  Dept: "Marketing",
-  Level: "Senior",
-  salary: 0,
-    
-  emp: function () {
-    // console.log("Employee ID : " + Emp3.EmpId())
-      console.log("Employee name : " + this.Name)
-      console.log("Employee salary : " + salaryemp1(1500,2000))
-  }
-  }
-    
-  Emp3.emp();
 
-const Emp4 = {
-  EmpId: function(){
-    id++;
-    return id;
-  },
-  Name: "Safi Walid	",
-  Dept: "Administration",
-  Level: "Mid-Senior",
-  salary: 0,
-    
-  emp: function () {
-    // console.log("Employee ID : " + Emp4.EmpId())
-    console.log("Employee name : " + this.Name)
-    console.log("Employee salary : " + salaryemp1(1000,1500))
-  }
-  }
-    
-  Emp4.emp();
+const allEmployee = [];
 
-    
-const Emp5 = {
-  EmpId:  function(){
-    id++;
-    return id;
-  },
-  Name: "Omar Zaid	",
-  Dept: "Development",
-  Level: "Senior",
-  salary: 0,
-    
-  emp: function () {
-    // console.log("Employee ID : " + Emp5.EmpId())
-    console.log("Employee name : " + this.Name)
-    console.log("Employee salary : " + salaryemp1(1500,2000))
-  }
-  }
-    
-  Emp5.emp();
+function Employee(empIdValue,  nameValue,  deptValue,  levelValue,  salaryValue ) 
+{
 
-const Emp6 = {
-  EmpId:  function(){
-    id++;
-    return id;
-  },
-  Name: "Rana Saleh",
-  Dept: "Development",
-  Level: "Junior",
-  salary: 0,
-      
-  emp: function () {
-    // console.log("Employee ID : " + Emp6.EmpId())
-    console.log("Employee name : " + this.Name)
-    console.log("Employee salary : " + salaryemp1(500,1000))
-  }
-  }
-      
-  Emp6.emp();
+  this.EmpId = empIdValue;
+  this.Name = nameValue;
+  this.Dept = deptValue;
+  this.Level = levelValue;
+  this.salary = salaryValue;
+  allEmployee.push(this);
 
-  const Emp7 = {
-    EmpId:  function(){
-      id++;
-      return id;
-    },
-    Name: "Hadi Ahmad",
-    Dept: "Finance",
-    Level: "Mid-Senior",
-    salary: 0,
-        
-    emp: function () {
-      // console.log("Employee ID : " + Emp7.EmpId())
-      console.log("Employee name : " + this.Name)
-      console.log("Employee salary : " + salaryemp1(1000,1500))
-    }
-    }
-        
-    Emp7.emp();
+  }
+
+
+  Employee.prototype.render = function () {
+    
+      console.log( `Employee name : ${this.Name}`);
+      console.log(`Department : ${this.Dept}`);
+      console.log("Employee salary :" + this.salary);
+      console.log("\n");
+
+  };
+
+
+const employee1 = new Employee(1000 , "Ghazi samer" , "Administration" , "Senior" , salaryemp(1500,2000) );
+const employee2 = new Employee(1001 , "Lana Ali" , "Finance" , "Senior" , salaryemp(1500,2000) );
+const employee3 = new Employee(1002 , "Tamara Ayoub" , "Marketing" , "Senior" , salaryemp(1500,2000) );
+const employee4 = new Employee(1003 , "Safi Walid" , "Administration" , "Mid-Senior" , salaryemp(1000,1500) );
+const employee5 = new Employee(1004 , "Omar Zaid" , "Development" , "Senior" , salaryemp(1500,2000) );
+const employee6 = new Employee(1005 , "Rana Saleh" , "Development" , "Junior" , salaryemp(500,1000) );
+const employee7 = new Employee(1006 , "Hadi Ahmad" , "Finance" , "Mid-Senior" , salaryemp(1000,1500) );
+
+
+
+  employee1.render();
+  employee2.render();
+  employee3.render();
+  employee4.render();
+  employee5.render();
+  employee6.render();
+  employee7.render();
+
