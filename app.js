75065f09dcac28c67391272f110bf81136ea3778
YOUR FULL NAMEomarali1997 <omar.aziz.9718@gmail.com>
2022-08-09 02:05:47 +0300

    ..

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
-
-
-function salaryemp(max, min) {
-
-  return Math.floor(Math.random() * (max - min)) + min;
+var net =0;
+function salaryemp(x){
+  if(x == "senior")
+  {
+    var fullsalary = Math.floor(Math.random() * (2000 - 1500)) + 1500;
+    var net = fullsalary - (fullsalary*0.075).toFixed(0);
+    return net;
+  }
+  else if(x == "mid-senior")
+  {
+    var fullsalary = Math.floor(Math.random() * (1500 - 1000)) + 1000;
+    var net = fullsalary - (fullsalary*0.075).toFixed(0);
+    return net;
+  }
+  else(x == "junior")
+  {
+    var fullsalary = Math.floor(Math.random() * (1000 - 500)) + 500;
+    var net = fullsalary - (fullsalary*0.075).toFixed(0);
+    return net;
+  }
 }
 
 var id = 999 ;
@@ -24,11 +39,11 @@ function Employee(empIdValue, nameValue, deptValue, levelValue, salaryValue, ima
   this.salary = salaryValue;
   this.image = imageValue;
   allEmployee.push(this);
-
 }
 
+var divE=document.getElementsByTagName("div");
 
-Employee.prototype.render = function () {
+Employee.prototype.render = function (index) {
   // document.write('<br>');
   // document.write(this.image);
   // document.write('<br>');
@@ -39,50 +54,52 @@ Employee.prototype.render = function () {
   // document.write(this.salary);
   // document.write('<br>');
 
-  const divEl = document.getElementsByClassName("omarClass");
-  console.log(divEl);
-
-
+  // const divEl = document.getElementsByClassName("omarClass");
+  // console.log(divEl);
+  
   const imgEl = document.createElement('img');
   imgEl.src=this.image;
   imgEl.alt=this.Name;
   body[0].appendChild(imgEl); 
 
 
-  const NIEl = document.createElement('Emp0'); //name,Id element
-  NIEl.textContent = `Employee name : ${this.Name} - ID: ${this.EmpId} "br" `;
+
+  const NIEl = document.createElement("p"); //name,Id element
+  NIEl.textContent = ` Name : ${this.Name} - ID: ${this.EmpId}   `;
   body[0].appendChild(NIEl);
   
 
-  const DLEl = document.createElement('Emp1'); //Dept , Level element
-  DLEl.textContent = `Department : ${this.Dept} - Level:${this.Level} "br" `;
+  const DLEl = document.createElement("p"); //Dept , Level element
+  DLEl.textContent = `Department : ${this.Dept} - Level:${this.level} `;
   body[0].appendChild(DLEl);
 
-  const SEl = document.createElement('Emp2'); //salary element
-  SEl.textContent = `${this.salary} "br" `;
+  const SEl = document.createElement("p"); //salary element
+  SEl.textContent = `${this.salary}  `;
   body[0].appendChild(SEl);
 
   
 
-  body[0].style.backgroundColor = "red";
+  // body[0].style.backgroundColor = "red";
 };
 
 
 
-const employee1 = new Employee(uniqeId(), "Ghazi samer", "Administration", "Senior", salaryemp(1500, 2000), "./img/Ghazi.jpg" );
-const employee2 = new Employee(uniqeId(), "Lana Ali", "Finance", "Senior", salaryemp(1500, 2000) , "./img/Lana.jpg" );
-const employee3 = new Employee(uniqeId(), "Tamara Ayoub", "Marketing", "Senior", salaryemp(1500, 2000) , "./img/Tamara.jpg" );
-const employee4 = new Employee(uniqeId(), "Safi Walid", "Administration", "Mid-Senior", salaryemp(1000, 1500) , "./img/Safi.jpg" );
-const employee5 = new Employee(uniqeId(), "Omar Zaid", "Development", "Senior", salaryemp(1500, 2000) , "./img/Omar.jpg") ;
-const employee6 = new Employee(uniqeId(), "Rana Saleh", "Development", "Junior", salaryemp(500, 1000) , "./img/Rana.jpg" );
-const employee7 = new Employee(uniqeId(), "Hadi Ahmad", "Finance", "Mid-Senior", salaryemp(1000, 1500) , "./img/Hadi.jpg" );
+const employee1 = new Employee(uniqeId(), "Ghazi samer" , "Administration", "Senior"    , salaryemp(), "./img/Ghazi.jpg"  );
+const employee2 = new Employee(uniqeId(), "Lana Ali"    , "Finance"       , "Senior"    , salaryemp(), "./img/Lana.jpg"   );
+const employee3 = new Employee(uniqeId(), "Tamara Ayoub", "Marketing"     , "Senior"    , salaryemp(), "./img/Tamara.jpg" );
+const employee4 = new Employee(uniqeId(), "Safi Walid"  , "Administration", "Mid-Senior", salaryemp(), "./img/Safi.jpg"   );
+const employee5 = new Employee(uniqeId(), "Omar Zaid"   , "Development"   , "Senior"    , salaryemp(), "./img/Omar.jpg"   );
+const employee6 = new Employee(uniqeId(), "Rana Saleh"  , "Development"   , "Junior"    , salaryemp(), "./img/Rana.jpg"   );
+const employee7 = new Employee(uniqeId(), "Hadi Ahmad"  , "Finance"       , "Mid-Senior", salaryemp(), "./img/Hadi.jpg"   );
 
 
-employee1.render();
-employee2.render();
-employee3.render();
-employee4.render();
-employee5.render();
-employee6.render();
-employee7.render();
+
+
+employee1.render(1);
+employee2.render(2);
+employee3.render(3);
+employee4.render(4);
+employee5.render(5);
+employee6.render(6);
+employee7.render(7);
 console.log(allEmployee);
